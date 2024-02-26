c3d76d9686636628d69efd7909e62dceb423c5af
darshit joshi <joshidarshit69@gmail.com>
2024-02-27 01:27:54 +0530

    added external api to expand prompt

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -9,6 +9,8 @@ import asyncio
 from fastapi.responses import StreamingResponse
 from starlette.websockets import WebSocketDisconnect
 
+import constants
+
 app = FastAPI()
 
 # Define the templates folder
@@ -20,7 +22,7 @@ websocket_connections = []
 # API endpoints
 GENERATE_API_ENDPOINT = "https://api.prodia.com/generate"
 IMAGE_API_ENDPOINT = "https://images.prodia.xyz"
-PROMPT_EXPAND_ENDPOINT = 'https://playground.com/api/expand-prompt'
+PROMPT_EXPAND_ENDPOINT = 'https://www.feedough.com/wp-admin/admin-ajax.php'
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -59,76 +61,56 @@ async def fetch_image(image_url):
 
         if image_response.status_code == 200:
             # If successful, return the image content
+            print("image created successfully, now plotting to front end")
             return image_response.content
         else:
             # If not successful, wait for 0.5 seconds before retrying
             await asyncio.sleep(0.5)
 
 
-model_map = {"absolutereality_v181 (hyper realism)": "absolutereality_v181.safetensors [3d9d4d2b]",
-             "Realistic_Vision_V5.0.safetensors (hyper realism)": "Realistic_Vision_V5.0.safetensors [614d1063]",
-             "amIReal_V41.safetensors (hyper realism)": "amIReal_V41.safetensors [0a8a2e61]",
-             "cyberrealistic_v33.safetensors (hyper realism)": "cyberrealistic_v33.safetensors [82b0d085]",
-             "edgeOfRealism_eorV20.safetensors (hyper realism)": "edgeOfRealism_eorV20.safetensors [3ed5de15]",
-             "ICantBelieveItsNotPhotography_seco.safetensors (hyper realism)": "ICantBelieveItsNotPhotography_seco.safetensors [4e7a3dfd]",
-             "epicrealism_naturalSinRC1VAE.safetensors (hyper realism)": "epicrealism_naturalSinRC1VAE.safetensors [90a4c676]",
-             "juggernaut_aftermath.safetensors (hyper realism)": "juggernaut_aftermath.safetensors [5e20c455]",
-             "majicmixRealistic_v4.safetensors (hyper realism)": "majicmixRealistic_v4.safetensors [29d0de58]",
-             "rundiffusionFX_v10.safetensors (hyper realism)": "rundiffusionFX_v10.safetensors [cd4e694d]",
-             "portraitplus_V1.0.safetensors (portrait)": "portraitplus_V1.0.safetensors [1400e684]",
-             "shoninsBeautiful_v10.safetensors (portrait)": "shoninsBeautiful_v10.safetensors [25d8c546]",
-             "lyriel_v16.safetensors (semi real)": "lyriel_v16.safetensors [68fceea2]",
-             "analog-diffusion-1.0.ckpt (nature)": "analog-diffusion-1.0.ckpt [9ca13f02]",
-             "rundiffusionFX25D_v10.safetensors (2.5d)": "rundiffusionFX25D_v10.safetensors [cd12b0ee]",
-             "anythingV5_PrtRE.safetensors (anime)": "anythingV5_PrtRE.safetensors [893e49b9]",
-             "AOM3A3_orangemixs.safetensors (anime)": "AOM3A3_orangemixs.safetensors [9600da17]",
-             "dalcefo_v4.safetensors (anime)": "dalcefo_v4.safetensors [425952fe]",
-             "mechamix_v10.safetensors (anime)": "mechamix_v10.safetensors [ee685731]",
-             "cetusMix_Version35.safetensors (artistic)": "cetusMix_Version35.safetensors [de2f2560]",
-             "elldreths-vivid-mix.safetensors (cartoon)": "elldreths-vivid-mix.safetensors [342d9d26]",
-             "childrensStories_v13D.safetensors (cartoon)": "childrensStories_v13D.safetensors [9dfaabcb]",
-             "revAnimated_v122.safetensors (cartoon)": "revAnimated_v122.safetensors [3f4fefd9]",
-             "meinamix_meinaV11.safetensors (cartoon)": "meinamix_meinaV11.safetensors [b56ce717]",
-             "childrensStories_v1SemiReal.safetensors (cartoon)": "childrensStories_v1SemiReal.safetensors [a1c56dbb]",
-             "dreamshaper_8.safetensors (cartoon)": "dreamshaper_8.safetensors [9d40847d]",
-             "toonyou_beta6.safetensors (cartoon)": "toonyou_beta6.safetensors [980f6b15]",
-             "childrensStories_v1ToonAnime.safetensors (anime)": "childrensStories_v1ToonAnime.safetensors [2ec7b88b]",
-             "Counterfeit_v30.safetensors (vintage cartoon)": "Counterfeit_v30.safetensors [9e2a8f19]",
-             "cuteyukimixAdorable_midchapter3.safetensors (vintage cartoon)": "cuteyukimixAdorable_midchapter3.safetensors [04bdffe6]",
-             "deliberate_v3.safetensors (dreamy)": "deliberate_v3.safetensors [afd9d2d4]",
-             "dreamlike-diffusion-1.0.safetensors (dreamy)": "dreamlike-diffusion-1.0.safetensors [5c9fd6e0]",
-             "neverendingDream_v122.safetensors [dreamy]": "neverendingDream_v122.safetensors [f964ceeb]",
-             "openjourney_V4.ckpt [2d]": "openjourney_V4.ckpt [ca2f377f]",
-             "pastelMixStylizedAnime_pruned_fp16.safetensors (2d horror) ": "pastelMixStylizedAnime_pruned_fp16.safetensors [793a26e8]",
-             }
-
-
 @app.get("/fetch_everything/")
 async def fetch_everything(prompt, tags, model):
+    print(f"received prompt: {prompt}, tags: {tags}, model: {model}")
     if not model:
         model = "absolutereality_v181.safetensors [3d9d4d2b]"
-    prompt += "8k image"
+    prompt += ", 8k image"
     for tag in tags:
-        prompt += ", " + tag
+        prompt += ", image style " + tag
 
-    # Define your headers
+    prompt += ", image style of " + constants.model_tag_map[model]
+
     headers = {
-        'Content-Type': 'application/json',
-        'Cookie': '__Host-next-auth.csrf-token=090a5ee7b3f9c3ee6ffa48166cf2a1fdd1d628712832811b9cc13df7ead7c820%7C45faa8af4e7e7b052c676809c0cad63bc2e3d4a05039e06d184aa89bbe78d3e2; __Secure-next-auth.callback-url=https%3A%2F%2Fplaygroundai.com; __Secure-next-auth.session-token=782b67b3-807f-4191-9543-7939d161b612'
+        'authority': 'www.feedough.com',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.7',
+        'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'cookie': 'ext_name=ojplmecpdpgccookcobabopnaifgidhf; _ga=GA1.1.2086675295.1708885051; aixg_user_id=aixg_65db843e334939.18757862; tve_leads_unique=1; tl_3314_3315_43=a%3A1%3A%7Bs%3A6%3A%22log_id%22%3BN%3B%7D; tl_3314_30184_75=a%3A1%3A%7Bs%3A6%3A%22log_id%22%3BN%3B%7D; _ga_722BX5RSV3=GS1.1.1708885050.1.1.1708885065.45.0.0; aixg_user_id=aixg_65db86a98b03d1.46449765',
+        'origin': 'https://www.feedough.com',
+        'referer': 'https://www.feedough.com/stable-diffusion-prompt-generator/',
+        'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Brave";v="122"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'sec-gpc': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
     }
 
-    # Define the payload
     data = {
-        'prompt': prompt
+        'action': 'aixg_generate',
+        'prompt': constants.DEFAULT_PROMPT + " - " + prompt + " - " + constants.DEFAULT_PROMPT_2,
+        '_ts': '1708885412291',
+        'form_id': 'ai_x_generator_65db28276d07f',
     }
-
     # Make the request with cookies and payload
-    response = requests.post(PROMPT_EXPAND_ENDPOINT, headers=headers, json=data)
+    response = requests.post(PROMPT_EXPAND_ENDPOINT, headers=headers, data=data)
 
     # Check the response status code
     if response.status_code == 200:
         # API request was successful
-        prompt = response.json()["expandedPrompt"]
+        prompt = response.json()["data"]["message"]
+        print(f"this is expanded prompt: {prompt}")
     else:
         # API request failed
         print('API request failed with status code:', response.status_code)
@@ -144,14 +126,14 @@ async def fetch_everything(prompt, tags, model):
         "seed": -1,
         "aspect_ratio": "square"
     }
-
+    print("generating image...")
     response = requests.get(GENERATE_API_ENDPOINT, params=params)
 
     if response.status_code == 200:
         # Extract the job ID from the response
         response_json = response.json()
         job_id = response_json.get("job")
-
+        print("image generated, now converting it to jpg...")
         # Use the job ID to construct the URL for the image API
         image_url = f"{IMAGE_API_ENDPOINT}/{job_id}.png?download=1"
 
